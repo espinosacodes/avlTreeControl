7c7038e87467456176655314152559df0ba9e69c
taxevader007 <santiagoespinosagiraldo1@gmail.com>
2024-04-26 09:29:15 -0500

    pushing proyect

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Car {
+
+    private String brand;
+    private String model;
+    private String plate;
+
+    public Car(String brand, String model, String plate) {
+        this.brand = brand;
+        this.model = model;
+        this.plate = plate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "brand=" + brand + ", model=" + model + ", plate=" + plate + '}';
+    }
+
+
+}
